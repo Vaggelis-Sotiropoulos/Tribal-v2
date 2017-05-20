2e0cb312f04d43451f37a029541db6e7d0db20fe
Carlo Las Marias <carlol@gmail.com>
2017-05-20 16:07:17 -0700

    styling changes

--- reactNative/components/Login.js
+++ reactNative/components/Login.js
@@ -33,40 +33,46 @@ class Login extends Component {
             style={{ width: '100%', height: '100%' }}
           />
         </View>
-        <View style={styles.login}>
 
-          <TextInput
-            style={styles.inputBox}
-            placeholder="username"
-            autoCapitalize="none"
-            onChangeText={text => this.setState({ username: text })}
-          />
+        <Image source={require('../public/images/recordPlayer.jpg')} style={styles.background}>
 
-          <TextInput
-            style={styles.inputBox}
-            secureTextEntry
-            placeholder="password"
-            onChangeText={text => this.setState({ password: text })}
-          />
 
-          <TouchableHighlight
-            activeOpacity={1}
-            style={styles.loginButton}
-            onPress={this.get.bind(this)}
-            title="Login with Spotify"
-          >
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableHighlight>
+          <View style={styles.login}>
 
-        </View>
-        <Image
-          source={require('../public/images/recordPlayer.gif')}
-          style={{ width: '100%', height: 200 }}
-        />
-        
-        <View style={styles.login}>
-          <Text></Text>
-        </View>
+            <TextInput
+              style={styles.inputBox}
+              placeholder="username"
+              autoCapitalize="none"
+              onChangeText={text => this.setState({ username: text })}
+            />
+
+            <TextInput
+              style={styles.inputBox}
+              secureTextEntry
+              placeholder="password"
+              onChangeText={text => this.setState({ password: text })}
+            />
+
+            <TouchableHighlight
+              activeOpacity={1}
+              style={styles.loginButton}
+              onPress={this.get.bind(this)}
+              title="Login with Spotify"
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableHighlight>
+
+          </View>
+
+          <Text style={{
+            fontFamily: 'Barrio',
+            marginTop: 160,
+            marginLeft: 175,
+            fontSize: 60,
+          }}
+          >Tribal
+          </Text>
+        </Image>
       </View>
     );
   }
